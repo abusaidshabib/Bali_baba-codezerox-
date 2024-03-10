a0959c5a567a4d925ca67b7194e06ab570f4a27b
Your Name <abusaidshabib712@gmail.com>
2024-03-11 00:30:34 +0600

    done

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function(){
-    fetch('https://dummyjson.com/products/1')
+    fetch("https://hexabazaar-abusaidshabib.vercel.app/products")
     .then(response => response.json())
     .then(data => {
         const productContainer = document.getElementById("products-container");
         productContainer.innerHTML = "";
 
-        data.forEach(product => {
+        data?.forEach(product => {
             console.log(product._id)
             let productElement = document.createElement('div');
             productElement.classList.add("shadow-2xl","rounded-2xl","p-5", "max-w-[384px]");
@@ -17,10 +17,18 @@ document.addEventListener("DOMContentLoaded", function(){
             </div>
             <p class="pt-2">${product.price}</p>
             <p>title: ${product.title}</p>
-            <a href="singleProduct.html" class="bg-primary text-white px-2 rounded">Buy Now</a>
+            <button class="buy-now-btn bg-primary text-white px-2 rounded" data-product-id="${product._id}">Buy Now</a>
             `;
             productContainer.appendChild(productElement);
         });
+
+        const buyNowButtons = document.querySelectorAll('.buy-now-btn')
+        buyNowButtons.forEach(button => {
+            button.addEventListener('click', function(){
+                const productId = this.getAttribute('data-product-id')
+                window.location.href = `singleProduct.html?id=${productId}`
+            })
+        })
     })
     .catch(error => {
         console.log(error)
---
+++ assets/js/singleProduct.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const params = new URLSearchParams(window.location.search)
+    const productId = params.get('id')
+    fetch(`https://hexabazaar-abusaidshabib.vercel.app/product/${productId}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        // const productContainer = document.getElementById("products-container");
+        // productContainer.innerHTML = "";
+
+        // data?.forEach(product => {
+        //     console.log(product._id)
+        //     let productElement = document.createElement('div');
+        //     productElement.classList.add("shadow-2xl","rounded-2xl","p-5", "max-w-[384px]");
+
+        //     productElement.innerHTML = `
+        //     <div class="h-96">
+        //         <img class="h-full w-full object-cover rounded-xl" src=${product.images[0]} alt="imran vai er fider" srcset="">
+        //     </div>
+        //     <p class="pt-2">${product.price}</p>
+        //     <p>title: ${product.title}</p>
+        //     <a href="singleProduct.html?id=${product._id}" class="buy-now-btn bg-primary text-white px-2 rounded" data-product-id="${product._id}">Buy Now</a>
+        //     `;
+        //     productContainer.appendChild(productElement);
+        });
+    })
+    .catch(error => {
+        console.log(error)
+    })
